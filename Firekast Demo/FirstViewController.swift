fb4e6d02f63863a504a5c5db8916a5024d8b1bab
François Rouault <francois.rouault@firekast.io>
2018-04-20 20:59:00 +0200

    Inital commit

---
+++ Firekast Demo/FirstViewController.swift
@@ -0,0 +1,25 @@
+//
+//  FirstViewController.swift
+//  Firekast Demo
+//
+//  Created by François Rouault on 20/04/2018.
+//  Copyright © 2018 Firekast. All rights reserved.
+//
+
+import UIKit
+
+class FirstViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+
+
+}
+
